d919fbae22468b3513ea08070e388fcf28cf9220
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2023-11-04 03:20:24 +0100

    install package toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,11 @@ import { Inter } from 'next/font/google'
 
 import type { Metadata } from 'next'
 import './globals.css'
+import Header from '@/components/Header'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
+import ToasterProvider from '@/providers/ToasterProvider'
 
 const font = Inter({ subsets: ['latin'] })
 
@@ -10,14 +15,19 @@ export const metadata: Metadata = {
   description: 'choir Vyrij',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <SupabaseProvider>
+          <ToasterProvider />
+          <UserProvider>
+            <ModalProvider />
+            <Header />
+            {children}
+          </UserProvider>
+        </SupabaseProvider>
+      </body>
     </html>
   )
 }
