6b80ba450ab8c4b0d0c21861a59784668dd52a3c
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2023-11-04 03:23:00 +0100

    create authentication modal and functionality

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+// import { ProductWithPrice } from '@/types'
+import AuthModal from '@/components/AuthModal'
+// import SubscribeModal from '@/components/SubscribeModal'
+// import UploadModal from '@/components/UploadModal'
+
+// interface ModalProviderProps {
+//   products: ProductWithPrice[];
+// }
+
+export default function ModalProvider() {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) return null
+
+  return (
+    <>
+      <AuthModal />
+      {/* <SubscribeModal products={products} />
+      <UploadModal /> */}
+    </>
+  )
+}
