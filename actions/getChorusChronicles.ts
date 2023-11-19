9ab7d4bef9aeb4bb3e719185f87d45796666d309
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2023-11-19 16:24:48 +0100

    implemented infinity scroll

--- actions/getChorusChronicles.ts
+++ actions/getChorusChronicles.ts
@@ -1,28 +1,74 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
+import { useEffect, useState } from 'react'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
 import { ChorusChronicles } from '@/types'
 
-const getChorusChronicles = async (): Promise<ChorusChronicles[]> => {
-  const supabase = createServerComponentClient({
-    cookies,
-  })
+const pageSize = 2
 
-  const { data, error } = await supabase
-    .from('chorus_chronicles')
-    .select(`
-    event_id,
-    title,
-    description,
-    date,
-    photos(href),
-    photos_event(photo_number)
-  `)
-  if (error) {
-    console.log(error.message)
+const supabase = createClientComponentClient()
+
+const getChorusChronicles = async (page:number): Promise<ChorusChronicles[]> => {
+  try {
+    const { data, error } = await supabase
+      .from('chorus_chronicles')
+      .select(`
+        event_id,
+        title,
+        description,
+        date,
+        photos(href),
+        photos_event(photo_number)
+      `)
+      .range(page, page + pageSize - 1)
+
+    if (error) {
+      // eslint-disable-next-line no-console
+      console.error('Error fetching data:', (error as unknown as Error).message)
+      return []
+    }
+
+    return data as ChorusChronicles[]
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Unexpected error:', (error as Error).message)
+    return []
   }
-
-  return (data as any) || []
 }
 
-export default getChorusChronicles
+export default function useChorusChronicles() {
+  const [dataSupabase, setDataSupabase] = useState<ChorusChronicles[]>([])
+  const [page, setPage] = useState<number>(1)
+  const [hasMoreData, setHasMoreData] = useState(true)
+  const [chronicles, setChronicles] = useState<ChorusChronicles[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const newData = await getChorusChronicles(page)
+        if (newData.length < pageSize) {
+          setHasMoreData(false)
+        }
+
+        setDataSupabase((prevData) => [...prevData, ...newData])
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error('Error fetching data:', (error as Error).message)
+      }
+    }
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    fetchData()
+  }, [page])
+
+  const loadMore = () => {
+    setPage((prevPage) => prevPage + 1)
+  }
+
+  useEffect(() => {
+    const newData = dataSupabase.filter((newItem) => !chronicles.some((oldItem) => oldItem.event_id === newItem.event_id))
+    if (newData.length > 0) {
+      setChronicles((prevChronicles) => [...prevChronicles, ...newData])
+    }
+  }, [chronicles, dataSupabase])
+
+  return { chronicles, loadMore, hasMoreData }
+}
--- app/chorusChronicles/page.tsx
+++ app/chorusChronicles/page.tsx
@@ -1,60 +1,77 @@
+'use client'
+
+import InfiniteScroll from 'react-infinite-scroll-component'
 import getChorusChronicles from '@/actions/getChorusChronicles'
+import { ChorusChronicles as ChorusChroniclesProps } from '@/types'
 import RenderPhotos from './RenderPhotos'
 
-export default async function ChorusChronicles() {
-  const eventsData = await getChorusChronicles()
+function ChorusChronicles() {
+  const { chronicles, loadMore, hasMoreData } = getChorusChronicles<ChorusChroniclesProps[]>()
 
-  const photoEventCounts = eventsData?.map((event) => ({
+  const photoEventCounts = chronicles.map((event) => ({
     event_id: event.event_id,
     count: event.photos_event?.length || 0,
   })) || []
 
   return (
-    <main className="container mx-auto">
-      <h1>ChorusChronicles</h1>
-      {eventsData.map(({
-        event_id, date, title, description, photos,
-      }) => (
-        <div key={event_id}>
-          <div>
-            <h2>{date}</h2>
-            <h1>{title}</h1>
-            <h2>{description}</h2>
-          </div>
+    <InfiniteScroll
+      dataLength={chronicles.length}
+      next={loadMore}
+      hasMore={hasMoreData}
+      loader={<h4>Loading...</h4>}
+    >
+      <main className="container mx-auto">
+        <h1>ChorusChronicles</h1>
+        {chronicles.map(({
+          event_id,
+          date,
+          title,
+          description,
+          photos,
+        }) => (
+          <div key={event_id}>
+            <div>
+              <h2>{date}</h2>
+              <h1>{title}</h1>
+              <h2>{description}</h2>
+            </div>
 
-          {photoEventCounts.find((item) => item.event_id === event_id)?.count === 1 && (
-            <div className="mt-10">
-              {RenderPhotos({ photos, className: 'w-full' })}
-            </div>
-          )}
-          {photoEventCounts.find((item) => item.event_id === event_id)?.count === 2 && (
-            <div className="mt-10 flex">
-              {RenderPhotos({ photos, limit: 1, className: '-ml-20' })}
-              {RenderPhotos({ photos, startFromIndex: 1 })}
-            </div>
-          )}
-          {photoEventCounts.find((item) => item.event_id === event_id)?.count === 3 && (
-            <div className="mt-10 flex">
-              <div className="flex-col">
+            {photoEventCounts.find((item) => item.event_id === event_id)?.count === 1 && (
+              <div className="mt-10">
+                {RenderPhotos({ photos, className: 'w-full' })}
+              </div>
+            )}
+            {photoEventCounts.find((item) => item.event_id === event_id)?.count === 2 && (
+              <div className="mt-10 flex">
                 {RenderPhotos({ photos, limit: 1, className: '-ml-20' })}
-                {RenderPhotos({ photos, startFromIndex: 1, limit: 1 })}
+                {RenderPhotos({ photos, startFromIndex: 1 })}
               </div>
-              {RenderPhotos({ photos, startFromIndex: 2 })}
-            </div>
-          )}
-          {photoEventCounts.find((item) => item.event_id === event_id)?.count === 4 && (
-            <div className="mt-10 flex">
-              <div className="flex-col">
-                {RenderPhotos({ photos, limit: 1, className: '-ml-20' })}
-                {RenderPhotos({ photos, startFromIndex: 1, limit: 1 })}
+            )}
+            {photoEventCounts.find((item) => item.event_id === event_id)?.count === 3 && (
+              <div className="mt-10 flex">
+                <div className="flex-col">
+                  {RenderPhotos({ photos, limit: 1, className: '-ml-20' })}
+                  {RenderPhotos({ photos, startFromIndex: 1, limit: 1 })}
+                </div>
+                {RenderPhotos({ photos, startFromIndex: 2 })}
               </div>
-              <div className="flex-col">
-                {RenderPhotos({ photos, startFromIndex: 2, limit: 2 })}
+            )}
+            {photoEventCounts.find((item) => item.event_id === event_id)?.count === 4 && (
+              <div className="mt-10 flex">
+                <div className="flex-col">
+                  {RenderPhotos({ photos, limit: 1, className: '-ml-20' })}
+                  {RenderPhotos({ photos, startFromIndex: 1, limit: 1 })}
+                </div>
+                <div className="flex-col">
+                  {RenderPhotos({ photos, startFromIndex: 2, limit: 2 })}
+                </div>
               </div>
-            </div>
-          )}
-        </div>
-      ))}
-    </main>
+            )}
+          </div>
+        ))}
+      </main>
+    </InfiniteScroll>
   )
 }
+
+export default ChorusChronicles
