aa947135535db41ef47d18b5e20c957ae5a8121a
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2023-11-25 20:42:56 +0100

    refactor decommissioned

--- app/chorusChronicles/page.tsx
+++ app/chorusChronicles/page.tsx
@@ -1,77 +1,31 @@
 'use client'
 
 import InfiniteScroll from 'react-infinite-scroll-component'
+
+import useModelData from '@/hooks/useModelData'
 import getChorusChronicles from '@/actions/getChorusChronicles'
 import { ChorusChronicles as ChorusChroniclesProps } from '@/types'
-import RenderPhotos from './RenderPhotos'
+import ChorusEventCard from './ChorusEventCard'
 
-function ChorusChronicles() {
-  const { chronicles, loadMore, hasMoreData } = getChorusChronicles<ChorusChroniclesProps[]>()
-
-  const photoEventCounts = chronicles.map((event) => ({
-    event_id: event.event_id,
-    count: event.photos_event?.length || 0,
-  })) || []
-
+export default function ChorusChronicles() {
+  const {
+    modelData,
+    loadMore,
+    hasMoreData,
+  } = useModelData<ChorusChroniclesProps>(getChorusChronicles, 2)
   return (
     <InfiniteScroll
-      dataLength={chronicles.length}
+      dataLength={modelData.length}
       next={loadMore}
       hasMore={hasMoreData}
       loader={<h4>Loading...</h4>}
+      style={{ height: '105vh' }}
     >
       <main className="container mx-auto">
-        <h1>ChorusChronicles</h1>
-        {chronicles.map(({
-          event_id,
-          date,
-          title,
-          description,
-          photos,
-        }) => (
-          <div key={event_id}>
-            <div>
-              <h2>{date}</h2>
-              <h1>{title}</h1>
-              <h2>{description}</h2>
-            </div>
-
-            {photoEventCounts.find((item) => item.event_id === event_id)?.count === 1 && (
-              <div className="mt-10">
-                {RenderPhotos({ photos, className: 'w-full' })}
-              </div>
-            )}
-            {photoEventCounts.find((item) => item.event_id === event_id)?.count === 2 && (
-              <div className="mt-10 flex">
-                {RenderPhotos({ photos, limit: 1, className: '-ml-20' })}
-                {RenderPhotos({ photos, startFromIndex: 1 })}
-              </div>
-            )}
-            {photoEventCounts.find((item) => item.event_id === event_id)?.count === 3 && (
-              <div className="mt-10 flex">
-                <div className="flex-col">
-                  {RenderPhotos({ photos, limit: 1, className: '-ml-20' })}
-                  {RenderPhotos({ photos, startFromIndex: 1, limit: 1 })}
-                </div>
-                {RenderPhotos({ photos, startFromIndex: 2 })}
-              </div>
-            )}
-            {photoEventCounts.find((item) => item.event_id === event_id)?.count === 4 && (
-              <div className="mt-10 flex">
-                <div className="flex-col">
-                  {RenderPhotos({ photos, limit: 1, className: '-ml-20' })}
-                  {RenderPhotos({ photos, startFromIndex: 1, limit: 1 })}
-                </div>
-                <div className="flex-col">
-                  {RenderPhotos({ photos, startFromIndex: 2, limit: 2 })}
-                </div>
-              </div>
-            )}
-          </div>
+        {modelData.map((event) => (
+          <ChorusEventCard key={event.event_id} event={event} />
         ))}
       </main>
     </InfiniteScroll>
   )
 }
-
-export default ChorusChronicles
