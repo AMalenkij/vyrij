248eb18b2c908a246c664f65eb813f806641c0bc
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-01-01 15:50:09 +0100

    update: no page request required

--- actions/getMajorEvent.ts
+++ actions/getMajorEvent.ts
@@ -2,18 +2,16 @@ import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
 import { MajorEvent } from '@/types'
 
-const pageSize = 1
-
 const supabase = createClientComponentClient()
 
-export default async function getMajorEvent(page:number): Promise<MajorEvent[]> {
+export default async function getMajorEvent(): Promise<MajorEvent[]> {
   try {
     const { data, error } = await supabase
       .from('major_event')
       .select(`
-      chorus_chronicles(title, date, photos(href))
+      minor_event(title, date, photos(href))
       `)
-      .range(page, page + pageSize - 1)
+      .order('minor_event(date)', { ascending: true }) // Order by date in ascending order (oldest first)
 
     if (error) {
       // eslint-disable-next-line no-console
