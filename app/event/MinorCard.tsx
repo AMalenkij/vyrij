c8f8cc5d08da2df24ea352af4aebf28d7a9a55c5
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-01-01 15:48:03 +0100

    rename and small change in code

---
+++ app/event/MinorCard.tsx
@@ -0,0 +1,65 @@
+import React, { forwardRef } from 'react'
+
+import { ChorusChronicles as ChorusChroniclesProps } from '@/types'
+import RenderPhotos from './RenderPhotos'
+
+interface ChorusEventCardProps {
+  event: ChorusChroniclesProps;
+}
+
+const MinorCard = forwardRef<HTMLDivElement, ChorusEventCardProps>(
+  ({ event }: ChorusEventCardProps, ref) => {
+    const {
+      date,
+      title,
+      description,
+      photos,
+    } = event
+
+    const photoEventCounts = event.photos_event?.length || 0
+
+    return (
+      <div ref={ref} className="flex flex-col gap-12">
+        <div>
+          <h2>{date}</h2>
+          <h1>{title}</h1>
+          <h2>{description}</h2>
+        </div>
+
+        {photoEventCounts === 1 && (
+          <div className="mt-10">
+            {RenderPhotos({ photos, className: 'w-full' })}
+          </div>
+        )}
+        {photoEventCounts === 2 && (
+          <div className="mt-10 flex">
+            {RenderPhotos({ photos, limit: 1, className: '-ml-20' })}
+            {RenderPhotos({ photos, startFromIndex: 1 })}
+          </div>
+        )}
+        {photoEventCounts === 3 && (
+          <div className="mt-10 flex">
+            <div className="flex-col">
+              {RenderPhotos({ photos, limit: 1, className: '-ml-20' })}
+              {RenderPhotos({ photos, startFromIndex: 1, limit: 1 })}
+            </div>
+            {RenderPhotos({ photos, startFromIndex: 2 })}
+          </div>
+        )}
+        {photoEventCounts === 4 && (
+          <div className="mt-10 flex">
+            <div className="flex-col">
+              {RenderPhotos({ photos, limit: 1, className: '-ml-20' })}
+              {RenderPhotos({ photos, startFromIndex: 1, limit: 1 })}
+            </div>
+            <div className="flex-col">
+              {RenderPhotos({ photos, startFromIndex: 2, limit: 2 })}
+            </div>
+          </div>
+        )}
+      </div>
+    )
+  },
+)
+
+export default MinorCard
