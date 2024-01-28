28b71bdfb26c55ed110e672ce63084fde3caf2c4
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-01-28 20:48:11 +0100

    Update page.tsx
    minor change

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,11 +1,17 @@
+import { useStore } from '@/state/photoMain'
 import HeroHeaders from '@/components/HeroHeaders'
 import FloatingImageGallery from './FloatingImageGallery'
+import imgHero from '@/public/img/Hero.jpg'
 
 export default function Home() {
+  const dataPhotoMain = useStore.getState().photoMain
   return (
     <main>
-      <FloatingImageGallery />
-      <div className="flex items-center justify-center h-screen"><HeroHeaders /></div>
+      <FloatingImageGallery data={dataPhotoMain}>
+        <div className="flex items-center justify-center h-screen">
+          <HeroHeaders imgSrc={imgHero} />
+        </div>
+      </FloatingImageGallery>
     </main>
   )
 }
