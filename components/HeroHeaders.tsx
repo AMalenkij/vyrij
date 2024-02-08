4512f79dba7145616163b5d583dfd8046cf3adb6
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-02-08 11:10:53 +0100

    Update HeroHeaders.tsx
    added animation from fm

--- components/HeroHeaders.tsx
+++ components/HeroHeaders.tsx
@@ -1,44 +1,119 @@
+'use client'
+
 import { twMerge } from 'tailwind-merge'
 import Image from 'next/image'
+import { motion } from 'framer-motion'
 
-import Button from '@/components/Button'
-import floating8 from '@/public/images/floating_8.jpg'
-import { HERO_TEXT, QUOTE_TEXT } from '@/constants/settings'
+import { HERO_TEXT, QUOTE_TEXT, AUTOR } from '@/constants/settings'
 
 interface HeroHeadersProps {
   className?: string
+  imgSrc: string
 }
-const time = 'Mar 10, 2020'
-const fish = 'Boost your conversion rate'
 
 export default function HeroHeaders({ imgSrc, className } : HeroHeadersProps) {
   return (
-    <div className={twMerge(
-      `
-     relative
-     flex
-     w-1/2
-        `,
-      className,
-    )}
+    <div className="
+    relative
+    flex
+    flex-col
+    md:flex-row
+    items-center
+    justify-center
+    p-30
+    md:p-52"
     >
-      <Image
-        src={imgSrc}
-        width={600}
-        height={600}
-        alt="Hero img"
-      />
-      <article className="flex flex-col justify-end p-4">
-        <time>{time}</time>
-        <h2>{fish}</h2>
-        <p className="line-clamp-3">{HERO_TEXT}</p>
-        <Button>
-          Explore
-        </Button>
-      </article>
-      <article className="absolute t-0 left-1/4">
-        <p className="text-4xl text-orange-500">{QUOTE_TEXT}</p>
-      </article>
+      <div className={twMerge(
+        `
+      md:flex
+      mx-auto
+      justify-center
+      w-7/12
+      `,
+        className,
+      )}
+      >
+        <motion.div
+          initial={{ opacity: 0, scale: 0.7, y: 100 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          transition={{
+            duration: 2,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+        >
+          <Image
+            src={imgSrc}
+            width={688} // 688 origin
+            height={459} // 459 origin
+            alt="Hero img"
+          />
+        </motion.div>
+        <div className="
+      md:w-1/3
+      md:flex-none
+      text-gray
+      "
+        >
+          <article className="
+        md:-ml-96
+        mb-10
+        md:mb-18
+        relative
+        md:mr-auto
+        mt-4
+        "
+          >
+            <motion.p
+              className="text-8xl font-comforter"
+              initial={{ opacity: 0, y: 70 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                duration: 2,
+                ease: [0, 0.71, 0.2, 1.01],
+              }}
+            >
+              {QUOTE_TEXT}
+            </motion.p>
+
+            <motion.p
+              className="text-2xl"
+              initial={{ opacity: 0, y: 40 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                duration: 2.5,
+                ease: [0, 0.71, 0.2, 1.01],
+              }}
+            >
+              {AUTOR}
+            </motion.p>
+          </article>
+          <article className="
+        max-w-380
+        md:ml-55
+        px-30
+        md:px-0
+        text-center
+        md:text-left
+        md:mr-auto
+        mb-10
+        "
+          >
+            <motion.p
+              className="ml-6 -mt-10"
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                duration: 2,
+                ease: [0, 0.71, 0.2, 1.01],
+              }}
+            >
+              {HERO_TEXT}
+            </motion.p>
+          </article>
+
+        </div>
+      </div>
     </div>
+
   )
 }
