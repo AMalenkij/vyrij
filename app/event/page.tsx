46e6a15046382d4cffdb9bd0fffe9a420831defa
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-02-19 22:19:25 +0100

    there will be no pogination by slug

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -1,14 +1,14 @@
 import { useStore } from '@/state/allData'
 import { GroupedEvent } from '@/types'
-import MinorCard from '../MinorCard'
-import MajorCard from '../MajorCard'
+import MinorCard from './MinorCard'
+import MajorCard from './MajorCard'
 
 export default function Event() {
   const { allEvent }: { allEvent: GroupedEvent[] } = useStore.getState()
   return (
     <section>
       {allEvent?.map((item) => (
-        <div key={item.date}>
+        <div key={item.date} className="mb-48">
           <MajorCard majorEvent={item} />
           {item?.minorEvents?.map((elm) => (
             <MinorCard minorEvent={elm} />
