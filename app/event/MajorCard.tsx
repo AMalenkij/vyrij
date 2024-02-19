9b528c0382ef853cecf30e29e34f725e333b7866
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-02-19 22:21:03 +0100

    Update MajorCard.tsx
    added navigation for major event
    date processing has also been removed

--- app/event/MajorCard.tsx
+++ app/event/MajorCard.tsx
@@ -6,33 +6,47 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 import RenderPhotos from './RenderPhotos'
 
 export interface MajorEvent {
-  date: string;
+  year: number;
   title: string;
   photos: { href: string }[];
 }
 
 export default function MajorCard({ majorEvent }:{ majorEvent:MajorEvent }) {
-  const { date, title, photos } = majorEvent
+  const { year, title, photos } = majorEvent
 
   const refFM = useRef<HTMLDivElement>(null)
 
   const { scrollYProgress } = useScroll({
     target: refFM,
-    offset: ['0', '1'],
+    offset: ['0', '1 0.9'],
   })
-  const scaleProgress = useTransform(scrollYProgress, [0, 1], [1, 1.25])
+  const scaleProgressImg = useTransform(scrollYProgress, [0, 1], [1, 1.25])
+  const scaleProgressText = useTransform(scrollYProgress, [0, 1], [1, 1.1])
   return (
     <motion.div
-      className="relative h-[200vh]"
+      className="relative h-[200vh] mb-96"
       ref={refFM}
       style={{
-        scale: scaleProgress,
+        scale: scaleProgressImg,
       }}
+      id={year.toString()}
     >
       <div className="sticky top-0 w-full h-screen overflow-hidden">
-        {RenderPhotos({ photos, limit: 1, className: 'w-full h-screen relative' })}
-        <div className="absolute inset-0 flex justify-center items-center text-white text-2xl font-bold pb-56 md:mb-5">{`${date.substring(0, 4)}р`}</div>
-        <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-9xl font-comforter">{title}</div>
+        <div>
+          {RenderPhotos({ photos, limit: 1, className: 'w-full h-screen relative' })}
+        </div>
+        <div className="absolute inset-0 flex justify-center items-center text-white text-2xl font-bold pb-56 md:mb-5">
+          {year}
+          p
+        </div>
+        <motion.div
+          className="absolute inset-0 flex flex-col justify-center items-center text-white text-9xl font-comforter"
+          style={{
+            scale: scaleProgressText,
+          }}
+        >
+          {title}
+        </motion.div>
       </div>
     </motion.div>
   )
