e4415680984ee350f7aa7e714bcb6b4f21dc764d
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-02-24 01:42:56 +0100

    Update MajorCard.tsx
    fixed errors caused by incorrect typing
    made a mobile version

--- app/event/MajorCard.tsx
+++ app/event/MajorCard.tsx
@@ -3,16 +3,11 @@
 import React, { useRef } from 'react'
 import { motion, useScroll, useTransform } from 'framer-motion'
 
+import { CustomMajorEvents } from '@/types'
 import RenderPhotos from './RenderPhotos'
 
-export interface MajorEvent {
-  year: number;
-  title: string;
-  photos: { href: string }[];
-}
-
-export default function MajorCard({ majorEvent }:{ majorEvent:MajorEvent }) {
-  const { year, title, photos } = majorEvent
+export default function MajorCard({ majorEvents }:{ majorEvents:CustomMajorEvents }) {
+  const { year, title, photos } = majorEvents
 
   const refFM = useRef<HTMLDivElement>(null)
 
@@ -20,27 +15,72 @@ export default function MajorCard({ majorEvent }:{ majorEvent:MajorEvent }) {
     target: refFM,
     offset: ['0', '1 0.9'],
   })
+
   const scaleProgressImg = useTransform(scrollYProgress, [0, 1], [1, 1.25])
   const scaleProgressText = useTransform(scrollYProgress, [0, 1], [1, 1.1])
+
   return (
     <motion.div
-      className="relative h-[200vh] mb-96"
+      className="
+      relative
+      2xl:mb-80
+      xl:mb-72
+      lg:mb-64
+      mb-60
+      "
       ref={refFM}
       style={{
         scale: scaleProgressImg,
+        height: '200vh',
       }}
       id={year.toString()}
     >
-      <div className="sticky top-0 w-full h-screen overflow-hidden">
+      <div className="
+      sticky
+      top-0
+      w-full
+      h-screen
+      overflow-hidden
+      "
+      >
         <div>
-          {RenderPhotos({ photos, limit: 1, className: 'w-full h-screen relative' })}
+          {RenderPhotos({ photos, limit: 1, className: 'min-w-full  max-w-full h-screen relative' })}
         </div>
-        <div className="absolute inset-0 flex justify-center items-center text-white text-2xl font-bold pb-56 md:mb-5">
+        <div className="
+        absolute
+        inset-0
+        flex
+        justify-center
+        items-center
+        text-white
+        2xl:text-2xl
+        lg:text-xl
+        text-lg
+        font-bold
+        2xl:pb-56
+        xl:pb-40
+        pb-32
+        md:mb-5"
+        >
           {year}
           p
         </div>
         <motion.div
-          className="absolute inset-0 flex flex-col justify-center items-center text-white text-9xl font-comforter"
+          className="
+          absolute
+          inset-0
+          flex
+          flex-col
+          justify-center
+          items-center
+          text-white
+          2xl:text-9xl
+          xl:text-8xl
+          lg:text-7xl
+          text-6xl
+          font-comforter
+          whitespace-normal
+          "
           style={{
             scale: scaleProgressText,
           }}
