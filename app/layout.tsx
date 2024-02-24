25d5b7c95691db56e7766cf7b6c81faa25c8f3be
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-02-24 01:45:39 +0100

    Update layout.tsx
    general ban on horizontal scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${comforter.variable}`}>
+      <body className={`${comforter.variable} overflow-x-hidden `}>
         <SupabaseProvider>
           <ToasterProvider />
           <UserProvider>
