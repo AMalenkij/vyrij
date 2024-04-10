d121b35ac10afcbae7f7e7dee6b5124e8f75aa1d
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-04-10 04:35:35 +0200

    Update page.tsx
    minor change

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -5,10 +5,10 @@ import MinorCard from './MinorCard'
 import MajorCard from './MajorCard'
 
 export default function Event() {
-  const allEvent = useStore.getState() as ModifiedMajorEvents[]
+  const { allEvent } = useStore.getState() as { allEvent: ModifiedMajorEvents[] }
   return (
     <div>
-      {allEvent?.map((item) => (
+      {allEvent.map((item) => (
         <section key={item.year} className="2xl:mb-44 xl:mb-40 lg:mb-32 md:mb-28 mb-24">
           <MajorCard majorEvents={item} />
           {item?.minorEvents?.map((elm) => (
