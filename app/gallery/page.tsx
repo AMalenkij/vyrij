1afb724176d1e17c16f4e305245e2ae38adf80f7
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-04-10 03:32:46 +0200

    Update page.tsx

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -6,7 +6,7 @@ export default async function Gallery() {
   return (
     <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        {photos.map((photo) => (
+        {photos?.map((photo) => (
           <BlurImage key={photo.photo_id} image={photo} />
         ))}
       </div>
