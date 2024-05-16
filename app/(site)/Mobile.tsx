d3cc378a3ed9cd565d09244db09493f06efc39ca
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-05-16 05:41:06 +0200

    create button link

--- app/(site)/Mobile.tsx
+++ app/(site)/Mobile.tsx
@@ -1,31 +1,27 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 import imgHero from '@/public/img/heroMobile.jpg'
 import { QUOTE_TEXT_NEXT, QUOTE_TEXT, AUTOR } from '@/constants/settings'
+import Compass from '@/public/svg/Compass'
 
 export default function Mobile() {
   return (
-    <article className="
-    min-h-screen
-    p-10
-    pointer-events-none
-    visible
-    lg:invisible
-    absolute
-    inset-0"
-    >
+    <div className="visible lg:invisible">
       <Image
         src={imgHero}
-        fill
+        height={900}
         alt="Hero img"
-        className="object-cover -z-10 "
+        className="object-cover -z-10 -mt-20 "
         quality={100}
       />
-      <div className="pt-44 text-center font-poiretOne text-white">
-        <p className="text-6xl pb-3">{QUOTE_TEXT}</p>
-        <p className="text-7xl pb-3 underline decoration-sky-500/30">{QUOTE_TEXT_NEXT}</p>
-        <p className="text-2xl">{AUTOR}</p>
-      </div>
-    </article>
+      <p className="text-5xl pb-3 text-end pt-5 px-3">{QUOTE_TEXT}</p>
+      <p className="text-5xl pb-3 underline decoration-sky-500/30 text-end px-3">{QUOTE_TEXT_NEXT}</p>
+      <p className="text-xl text-end px-3">{AUTOR}</p>
+      <Link className="text-xl text-center pb-3 absolute bottom-0 inset-x-0 flex items-center justify-center gap-2" href="/timeline" type="button">
+        Explore
+        <Compass />
+      </Link>
+    </div>
   )
 }
