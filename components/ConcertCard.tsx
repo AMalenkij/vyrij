f36caa05261a01cd85b56643ba0ac940c7eec6d5
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-07-11 19:01:09 +0200

    Update ConcertCard.tsx
    small change style

--- components/ConcertCard.tsx
+++ components/ConcertCard.tsx
@@ -12,13 +12,13 @@ export default function ConcertCard({
   return (
     <a href={link} className="group">
       <div className="flex  mx-auto mb-7">
-        <div className="lg:flex basis-1/6 lg:gap-x-10 grid gap-y-1 text-center lg:mr-0 lg:pl-10 mr-3">
-          <div className="text-5xl basis-1/4 text-center">{date}</div>
+        <div className="lg:flex items-start basis-1/6 lg:gap-x-10 grid gap-y-1 lg:mr-0 lg:pl-10 mr-3">
+          <div className="text-5xl basis-1/4">{date}</div>
           <div className="gap-y-1 grid">
             <div className="basis-1/4 text-xl">{month}</div>
             <div className="basis-1/4">{year}</div>
           </div>
-          <div className="basis-1/4 text-center">{time}</div>
+          <div className="basis-1/4">{time}</div>
         </div>
         <div className="lg:flex lf:gap-x-20 lg:basis-full grid gap-y-1">
           <h2 className="lg:basis-4/5 lg:text-center text-2xl lg:font-bold lg:text-xl lg:px-10">{title}</h2>
