f5d49986518ea9d8a66dd5419a33da611470edfc
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-07-13 02:24:27 +0200

    Update Mobile.tsx
    replaced the text with a constant

--- app/(site)/Mobile.tsx
+++ app/(site)/Mobile.tsx
@@ -2,7 +2,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import imgHero from '@/public/img/heroMobile.jpg'
-import { QUOTE_TEXT_NEXT, QUOTE_TEXT, AUTOR } from '@/constants/settings'
+import {
+  QUOTE_TEXT_NEXT, QUOTE_TEXT, AUTOR, EXPLORE,
+} from '@/constants/settings'
 import Compass from '@/public/svg/Compass'
 
 export default function Mobile() {
@@ -16,10 +18,10 @@ export default function Mobile() {
         quality={100}
       />
       <p className="text-5xl pb-3 text-end pt-5 px-3">{QUOTE_TEXT}</p>
-      <p className="text-5xl pb-3 underline decoration-sky-500/30 text-end px-3">{QUOTE_TEXT_NEXT}</p>
+      <p className="text-5xl pb-3 underline decoration-border text-end px-3">{QUOTE_TEXT_NEXT}</p>
       <p className="text-xl text-end px-3">{AUTOR}</p>
-      <Link className="text-xl text-center pb-3 absolute bottom-0 inset-x-0 flex items-center justify-center gap-2" href="/timeline" type="button">
-        Explore
+      <Link className="text-xl text-center pb-3 absolute bottom-0 inset-x-0 flex items-center justify-center gap-2" href="/timeline" type="button" prefetch>
+        {EXPLORE}
         <Compass />
       </Link>
     </div>
