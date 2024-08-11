b8831615a0aad13fa9236d0b4454779fe0600e41
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-08-11 02:05:36 +0200

    small style change

--- components/Title.tsx
+++ components/Title.tsx
@@ -4,11 +4,15 @@ import { twMerge } from 'tailwind-merge'
 export default function Title({ className, children } : { className?: string, children: React.ReactNode }) {
   return (
     <h2 className={twMerge(`
-      text-lg
-      sm:text-2xl
-      md:text-3xl
+      mt-28
+      lg:mt-36
+      text-3xl
+      sm:text-5xl
+      md:text-6xl
       text-center
-      my-2
+      mb-12
+      sm:mb-10
+      font-secondaryFont
     `, className)}
     >
       {children}
