6ed02bb499cc8999fa2a2a8e7e9a093deb760448
Anton Malenkij <itottoro@gmail.com>
2025-05-02 07:37:03 +0200

    Create routes.ts

---
+++ constants/routes.ts
@@ -0,0 +1,5 @@
+export const HOME_ROUTE = "/";
+export const TIMELINE_ROUTE = "/timeline";
+export const EVENT_ROUTE = "/event";
+export const CONCERTS_ROUTE = "/concerts";
+export const GALLERY_ROUTE = "/gallery";
