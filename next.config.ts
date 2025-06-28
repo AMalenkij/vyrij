0ae499097e7f6b362b28b1d77690cdef5f97bd36
Anton Malenkij <itottoro@gmail.com>
2025-06-28 17:42:05 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    formats: ["image/webp"],
+    // imageSizes: [200, 400, 600, 800, 1000, 1200, 1400],
+    // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
+      },
+      {
+        protocol: "https",
+        hostname: "www.youtube.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
       },
     ],
   },
