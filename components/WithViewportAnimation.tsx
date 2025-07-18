c2ea0440e2acbb14853178a17e1ae44fbff7031a
Anton Malenkij <itottoro@gmail.com>
2025-07-18 17:12:16 +0200

    Allow WithViewportAnimation to accept ReactNode children
    Changed the type of the children prop from JSX.Element to ReactNode in WithViewportAnimation to support a wider range of child elements.

--- components/WithViewportAnimation.tsx
+++ components/WithViewportAnimation.tsx
@@ -1,11 +1,11 @@
 "use client";
-
 import { motion } from "framer-motion";
+import { ReactNode } from "react";
 
 export default function WithViewportAnimation({
   children,
 }: {
-  children: JSX.Element;
+  children: ReactNode;
 }) {
   return (
     <motion.div
