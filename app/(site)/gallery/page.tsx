1c633f4a83675f75180f69911ec8aed167751201
Anton Malenkij <itottoro@gmail.com>
2025-07-19 17:01:47 +0200

    Filter out photos with null imageUrl in gallery
    Added a filter to exclude photos with a null imageUrl to ensure type safety and prevent rendering issues. Updated GalleryModal to use the filtered data.

--- app/(site)/gallery/page.tsx
+++ app/(site)/gallery/page.tsx
@@ -1,12 +1,8 @@
-// import { ClientCldImage } from "@/components/clientCldImage";
-// import { GalleryModal } from "@/components/GalleryModal";
-
 import Link from "next/link";
 import { sanityFetch } from "@/sanity/lib/live";
 import { galleryPhotosQuery } from "@/sanity/lib/queries";
 import Image from "next/image";
 import { GalleryModal } from "@/components/GalleryModal";
-// import { Badge } from "@/components/ui/badge";
 
 const Gallery = async ({
   searchParams,
@@ -16,9 +12,15 @@ const Gallery = async ({
   const { photoId } = await searchParams;
   const { data } = await sanityFetch({ query: galleryPhotosQuery });
 
-  // console.log(data);
+  // Filter out items where imageUrl is null and assert the type
+  // Надстройка для прохождения тпизации
+  const filteredData = data.filter(
+    (photo): photo is { _id: string; imageUrl: string } =>
+      photo.imageUrl !== null,
+  );
+
   return (
-    <div className="container mt-14">
+    <div className="container mx-auto mt-14">
       <main className="mt-20 w-full max-w-[1960px]">
         <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
           {data.map(({ imageUrl, _id }) => (
@@ -31,25 +33,17 @@ const Gallery = async ({
                 <Image
                   width={720}
                   height={480}
-                  src={imageUrl}
+                  src={imageUrl || ""}
                   alt="Gallery image"
                   sizes="(max-width: 640px) 100vw, (max-width: 1280px) 50vw, (max-width: 1536px) 33vw, 25vw"
                   className="transform rounded-lg transition will-change-auto md:brightness-90 md:group-hover:brightness-110"
                 />
-                {/* {post?.title_en && (
-                  <Badge
-                    variant="outline"
-                    className="absolute right-3 bottom-4 bg-stone-900/60 text-stone-50"
-                  >
-                    {post.title_en}
-                  </Badge>
-                )} */}
               </Link>
             </div>
           ))}
         </div>
       </main>
-      {photoId && <GalleryModal images={data} photoId={photoId} />}
+      {photoId && <GalleryModal images={filteredData} photoId={photoId} />}
     </div>
   );
 };
