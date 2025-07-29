aa33ecdfccf169ea834e9808bcb7b28356782906
Anton Malenkij <itottoro@gmail.com>
2025-07-29 04:43:07 +0200

    Refactor SubHeader props and styles
    Made sectionName a required prop and removed unused icon and Promise types from counter. Updated spacing and typography classes for improved layout and consistency.

--- components/SubHeader.tsx
+++ components/SubHeader.tsx
@@ -1,10 +1,9 @@
 import type { FC } from "react";
 
 type SubHeaderProps = {
-  sectionName?: string;
+  sectionName: string;
   title: string;
-  counter?: string | number | Promise<string | number>;
-  icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  counter?: string | number;
 };
 
 const SubHeader: FC<SubHeaderProps> = ({ sectionName, title, counter }) => {
@@ -13,18 +12,20 @@ const SubHeader: FC<SubHeaderProps> = ({ sectionName, title, counter }) => {
       aria-labelledby={title}
       className="flex w-full justify-between py-12 text-white"
     >
-      <div className="w-full space-y-1 ">
+      <div className="w-full space-y-3 ">
         <p className="text-muted-foreground text-xs sm:text-sm md:ml-3.5 lg:ml-3.5">
-          / {sectionName}
+          {sectionName}
         </p>
         <div className="flex w-full items-center justify-between">
           <div className="flex">
-            <h1 className="font-normal text-6xl uppercase tracking-tight sm:text-7xl md:text-9xl">
+            <h1 className="font-accent text-6xl sm:text-7xl md:text-8xl">
               {title}
             </h1>
             <div className="invisible flex flex-col items-center gap-y-1 sm:visible md:gap-y-3.5">
               <p className="p-0.5 font-light text-lg sm:p-1 md:p-3.5">
-                [ {counter} ]
+                {"{ "}
+                {counter}
+                {" }"}
               </p>
             </div>
           </div>
