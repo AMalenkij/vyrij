5446cf47787e1f41e3d9aa764514320bc18654b7
Anton Malenkij <itottoro@gmail.com>
2025-08-03 20:25:19 +0200

    small fix

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,5 +1,5 @@
 export const HOME_ROUTE = "/";
 export const TIMELINE_ROUTE = "/timeline";
-export const EVENT_ROUTE = "/event";
+export const EVENT_ROUTE = "/events";
 export const CONCERTS_ROUTE = "/concerts";
 export const GALLERY_ROUTE = "/gallery";
