7a7aebe9bdd5e151ef9c73d07ad7880842f2dc06
Anton Malenkij <itottoro@gmail.com>
2025-08-07 03:32:13 +0200

    Update routes.ts
    changed: paths are now stored in the object

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,5 +1,6 @@
-export const HOME_ROUTE = "/";
-export const TIMELINE_ROUTE = "/timeline";
-export const EVENT_ROUTE = "/events";
-export const CONCERTS_ROUTE = "/concerts";
-export const GALLERY_ROUTE = "/gallery";
+export const ROUTES_CONFIG = {
+  HOME: "/",
+  EVENTS: "/events",
+  CONCERTS: "/concerts",
+  GALLERY: "/gallery",
+} as const;
