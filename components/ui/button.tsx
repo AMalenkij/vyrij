f38dc55f43988bf4d39e4a3d842cf13f43412e2e
Anton Malenkij <itottoro@gmail.com>
2025-08-07 03:32:54 +0200

    Update button.tsx
    added uppercase

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         linkDefault: "text-primary underline-offset-4 hover:underline",
-        link: "relative inline-block text-white leading-8 no-underline transition-colors duration-300 before:content-[''] before:absolute before:bottom-[-0.5rem] before:left-0 before:w-full before:h-0.5 before:bg-white before:rounded-sm before:opacity-0 before:transition-all before:duration-400 hover:before:opacity-100 hover:before:transform hover:before:-translate-y-1",
+        link: "relative uppercase inline-block text-white leading-8 no-underline transition-colors duration-300 before:content-[''] before:absolute before:bottom-[-0.5rem] before:left-0 before:w-full before:h-0.5 before:bg-white before:rounded-sm before:opacity-0 before:transition-all before:duration-400 hover:before:opacity-100 hover:before:transform hover:before:-translate-y-1",
       },
       size: {
         default: "h-9 py-2 has-[>svg]:px-3",
