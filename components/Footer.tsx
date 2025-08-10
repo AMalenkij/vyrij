037b4dd1cdac2bfcb10f7e295f5256ab70c161b9
Anton Malenkij <itottoro@gmail.com>
2025-08-10 17:37:54 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -40,7 +40,7 @@ export default function Footer() {
         clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)",
       }}
     >
-      <footer className="fixed bottom-0 h-screen w-full bg-gradient-to-b from-red-500 to-red-600/50 px-6 pb-4 md:pb-6">
+      <footer className="glass-surface fixed bottom-0 h-screen w-full bg-gradient-to-b from-red-500/60 to-red-600/30 px-6 pb-4 md:pb-6">
         <div className="container mx-auto mt-14 text-white md:mt-32">
           {/* Main content grid */}
           <div className="grid grid-cols-1 gap-8 md:grid-cols-4 md:gap-12">
