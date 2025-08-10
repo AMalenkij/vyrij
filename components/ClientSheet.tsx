a104f137fab20b3423f5ad686cfb5aa9e3504a83
Anton Malenkij <itottoro@gmail.com>
2025-08-10 20:09:11 +0200

    Update ClientSheet.tsx

--- components/ClientSheet.tsx
+++ components/ClientSheet.tsx
@@ -27,7 +27,7 @@ export default function ClientSheet({
           <Menu className="h-10 w-10" />
         </Button>
       </SheetTrigger>
-      <SheetContent className="translate-custom-translate-rotate transform text-stone-50 transition duration-custom-long ease-custom-cubic">
+      <SheetContent className="translate-custom-translate-rotate transform text-foreground transition duration-custom-long ease-custom-cubic">
         {children}
       </SheetContent>
     </Sheet>
