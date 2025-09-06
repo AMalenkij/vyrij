341609ab6958f26d1aaf10f3a3859563f27a0921
Anton Malenkij <itottoro@gmail.com>
2025-09-06 21:19:43 +0200

    Add GradualBlur animation and integrate in Header
    Introduces a reusable GradualBlur animation component for top, bottom, left, or right blur effects with customizable strength, curve, and responsiveness. Integrates GradualBlur into the Header for a visually enhanced background effect above the navigation bar.

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,6 +11,7 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import SocialMediaSection from "@/components/SocialLink";
+import GradualBlur from "@/components/animation/GradualBlur";
 
 export default function Header() {
   const tNavMenu = useTranslations("Nav");
@@ -43,60 +44,84 @@ export default function Header() {
   ];
 
   return (
-    <header className="container sticky inset-x-1 top-0 z-50 mx-auto flex items-center justify-between px-2 py-2 md:px-0 md:py-4">
-      <Logo />
-      <nav className="hidden items-center space-x-2 md:flex">
-        <Navigation routes={routes} className="hidden md:flex" />
-        <div className="flex items-center space-x-2 pl-4">
-          <ModeToggle
-            variant="dropdown"
-            lightLabel={tModeToggle("lightLabel")}
-            darkLabel={tModeToggle("darkLabel")}
-            systemLabel={tModeToggle("systemLabel")}
-            toggleTheme={tModeToggle("toggleTheme")}
-          />
+    <>
+      <section
+        style={{
+          position: "fixed",
+          overflow: "hidden",
+          height: "5rem",
+          zIndex: 40,
+        }}
+        className="h-12 w-full bg-gradient-to-b from-background/30 to-background/0 md:h-20"
+      >
+        <GradualBlur
+          target="page"
+          position="top"
+          strength={4}
+          divCount={8}
+          curve="bezier"
+          exponential={true}
+          opacity={1}
+          className="relative inset-0 h-12 overflow-hidden md:h-20"
+        />
+      </section>
+      <header className="container sticky inset-x-1 top-0 z-50 mx-auto flex items-center justify-between px-2 py-2 md:px-0 md:py-4">
+        <Logo />
+        <nav className="hidden items-center space-x-2 md:flex">
+          <Navigation routes={routes} className="hidden md:flex" />
+          <div className="flex items-center space-x-2 pl-4">
+            <ModeToggle
+              variant="dropdown"
+              lightLabel={tModeToggle("lightLabel")}
+              darkLabel={tModeToggle("darkLabel")}
+              systemLabel={tModeToggle("systemLabel")}
+              toggleTheme={tModeToggle("toggleTheme")}
+            />
+            <LanguageToggle
+              variant="dropdown"
+              englishLabel={tLanguageToggle("english")}
+              polishLabel={tLanguageToggle("polski")}
+              ukrainianLabel={tLanguageToggle("ukrainian")}
+              changeLanguageLabel={tLanguageToggle("changeLanguage")}
+            />
+          </div>
+        </nav>
+        <ClientSheet>
+          <SheetHeader>
+            <SheetTitle>
+              <Logo />
+            </SheetTitle>
+            <SheetDescription>
+              {tMobileMenu("SheetDescription")}
+            </SheetDescription>
+          </SheetHeader>
+
           <LanguageToggle
-            variant="dropdown"
+            variant="accordion"
             englishLabel={tLanguageToggle("english")}
             polishLabel={tLanguageToggle("polski")}
             ukrainianLabel={tLanguageToggle("ukrainian")}
             changeLanguageLabel={tLanguageToggle("changeLanguage")}
           />
-        </div>
-      </nav>
-      <ClientSheet>
-        <SheetHeader>
-          <SheetTitle>
-            <Logo />
-          </SheetTitle>
-          <SheetDescription>{tMobileMenu("SheetDescription")}</SheetDescription>
-        </SheetHeader>
+          <ModeToggle
+            variant="accordion"
+            lightLabel={tModeToggle("lightLabel")}
+            darkLabel={tModeToggle("darkLabel")}
+            systemLabel={tModeToggle("systemLabel")}
+            toggleTheme={tModeToggle("toggleTheme")}
+          />
 
-        <LanguageToggle
-          variant="accordion"
-          englishLabel={tLanguageToggle("english")}
-          polishLabel={tLanguageToggle("polski")}
-          ukrainianLabel={tLanguageToggle("ukrainian")}
-          changeLanguageLabel={tLanguageToggle("changeLanguage")}
-        />
-        <ModeToggle
-          variant="accordion"
-          lightLabel={tModeToggle("lightLabel")}
-          darkLabel={tModeToggle("darkLabel")}
-          systemLabel={tModeToggle("systemLabel")}
-          toggleTheme={tModeToggle("toggleTheme")}
-        />
+          {/* Nav */}
+          <div className="mb-10 space-y-1 md:space-y-4">
+            <h3 className="font-medium text-sm uppercase tracking-wide opacity-80">
+              {tFooter("navigation.title")}
+            </h3>
+            <Navigation routes={routes} className="flex flex-col" />
+          </div>
 
-        {/* Nav */}
-        <div className="mb-10 space-y-1 md:space-y-4">
-          <h3 className="font-medium text-sm uppercase tracking-wide opacity-80">
-            {tFooter("navigation.title")}
-          </h3>
-          <Navigation routes={routes} className="flex flex-col" />
-        </div>
-
-        <SocialMediaSection />
-      </ClientSheet>
-    </header>
+          <SocialMediaSection />
+        </ClientSheet>
+      </header>
+    </>
   );
 }
