eb64077226abafd0701300be922335107cbf4754
Anton Malenkij <itottoro@gmail.com>
2025-11-19 06:38:22 +0100

    Add alt prop for YouTubeVideo iframe title

--- components/YouTubeVideo.tsx
+++ components/YouTubeVideo.tsx
@@ -2,16 +2,18 @@ import { twMerge } from "tailwind-merge";
 
 export default function YouTubeVideo({
   url,
+  alt,
   className,
 }: {
   url: string;
+  alt: string;
   className?: string;
 }) {
   return (
     <iframe
       className={twMerge("aspect-video w-full pt-8", className)}
+      title={alt}
       src={url}
-      title="YouTube video player"
       allowFullScreen
     />
   );
