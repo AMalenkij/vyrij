d06ce3cbb730b02de46bd7f52c5f8bc65e6f40a6
Anton Malenkij <itottoro@gmail.com>
2025-11-19 09:04:32 +0100

    Refactor toAppEvent: update date type and fallback logic

--- adapters/toAppEvent.ts
+++ adapters/toAppEvent.ts
@@ -1,7 +1,7 @@
 import type { EventQueryResult } from "@/sanity.types";
 import toAppEventTitle from "@/adapters/toAppEventTitle";
-import toAppTime from "@/adapters/toAppTime";
 import type { PortableTextBlock } from "@portabletext/types";
+import toPortableText from "./toPortableText";
 
 type RawEvent = EventQueryResult;
 
@@ -9,8 +9,7 @@ export interface AppEvent {
   id: string;
   title: string;
   description: PortableTextBlock[];
-  date: string;
-  time: string;
+  date: Date;
   tags: {
     id: string;
     name: string;
@@ -19,28 +18,8 @@ export interface AppEvent {
 
 export type EventAdapterTranslations = {
   noTitle: string;
-  noTime: string;
   noDescription: string;
 };
-
-function createFallbackDescription(text: string): PortableTextBlock[] {
-  return [
-    {
-      _type: "block",
-      _key: "fallback-block-key", // Ключ може бути довільним
-      style: "normal",
-      markDefs: [],
-      children: [
-        {
-          _type: "span",
-          _key: "fallback-span-key",
-          marks: [],
-          text: text, // noDescription
-        },
-      ],
-    },
-  ];
-}
 
 export default function toAppEvent(
   rawData: RawEvent,
@@ -51,19 +30,17 @@ export default function toAppEvent(
   }
 
   const adaptedTitle = toAppEventTitle(rawData.title, translations.noTitle);
-  const adaptedTime = toAppTime(rawData.time, translations.noTime);
-
+  const parsedDate = new Date(rawData.date);
   const description =
     rawData.description && rawData.description.length > 0
       ? rawData.description // 1. Використовуємо реальні дані, якщо вони є
-      : createFallbackDescription(translations.noDescription); // 2. Або створюємо заглушку
+      : toPortableText(translations.noDescription); // 2. Або створюємо заглушку
 
   return {
     id: rawData.id,
     title: adaptedTitle,
     description: description as PortableTextBlock[],
-    date: rawData.date,
-    time: adaptedTime,
+    date: parsedDate,
     tags: rawData.tags,
   };
 }
